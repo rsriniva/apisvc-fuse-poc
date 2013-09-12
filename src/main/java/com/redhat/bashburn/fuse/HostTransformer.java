b9b58d8b566ed869f63269041229072b45460f9d
Brian Ashburn <bashburn@redhat.com>
2013-09-12 11:45:41 -0400

    Adding documentation and making the transformer bean better

--- src/main/java/com/redhat/bashburn/fuse/HostTransformer.java
+++ src/main/java/com/redhat/bashburn/fuse/HostTransformer.java
@@ -1,7 +1,20 @@
 package com.redhat.bashburn.fuse;
 
 public class HostTransformer {
+	private String from;
+	private String to;
+
 	public String doTransform(String body) {
-		return body.replaceAll("localhost:8181/cxf", "localhost:8282/apimanager");
+		return body.replaceAll(getFrom(), getTo());
+	}
+
+	public String getFrom() { return this.from; }
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() { return this.to; }
+	public void setTo(String to) {
+		this.to = to;
 	}
 }
